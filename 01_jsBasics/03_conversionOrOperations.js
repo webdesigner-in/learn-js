b00a16f2ccab41666ab30b064e8059f7738603cf
Mohit Sharma <mp7242442@gmail.com>
2024-10-13 05:21:58 +0000

    conversions in javascript

---
+++ 01_jsBasics/03_conversionOrOperations.js
@@ -0,0 +1,7 @@
+// **************conversion****************
+let score = "333";
+console.log(typeof score)
+let scoreInNumber = Number(score)
+console.log(scoreInNumber)
+console.log(typeof scoreInNumber)
+
