878aa34789468d8ea8ff7bfe77ad874d45e3a926
Mohit Sharma <mp7242442@gmail.com>
2024-10-13 04:53:43 +0000

    variables used in javascript

---
+++ 01_jsBasics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553;
+let accountEmail = "email@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword , accountCity, accountState]);
