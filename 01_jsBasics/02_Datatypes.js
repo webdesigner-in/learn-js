7516240d5685d0a9105a999eccbc313689daa819
Mohit Sharma <mp7242442@gmail.com>
2024-10-13 05:01:26 +0000

    datatypes in javascript

---
+++ 01_jsBasics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let name = "mohit";
+let age = 20;
+let isLoggedIn = false;
+
+
+// Datatypes in Javascript
+/*
+Number,
+bigint
+string
+boolean
+null = it is a standalone value
+undefined
+symbol = whenever we talk about uniques we prefer symbol in javascript
+object
+*/
+
+console.table([name, age, isLoggedIn]);
