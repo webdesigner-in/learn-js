0e76a9ea1b2494e96368dc4e43113cc4756dc933
Mohit Sharma <mp7242442@gmail.com>
2024-10-15 07:46:07 +0000

    scopes in js

---
+++ 05_Scopes/01_scopes.js
@@ -0,0 +1,26 @@
+let a = 100;
+const b = 200;
+var c = 30;
+// console.log("The value of a , b , c -> ", a, b,c)
+
+
+if (true) {
+    let a = 10;
+    let b = 20;
+    // console.log("The value of inner a is" , a );
+}
+
+
+// console.log("The value of outer a is",a)
+
+
+function one() {
+    const username = 'mohit'
+    console.log(username)
+
+    function two() {
+        const website = 'google'
+        console.log(website)
+    }
+
+}
