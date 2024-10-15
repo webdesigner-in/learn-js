6b10e0e028c8f54a7a0d90d0a911c96586621c5b
Mohit Sharma <mp7242442@gmail.com>
2024-10-15 07:28:59 +0000

    functions with objects and arrays

---
+++ 04_Functions/02_functions.js
@@ -0,0 +1,24 @@
+function calculateCartPrice(...allprices){
+ return allprices;
+}
+
+// console.log(calculateCartPrice(200,300,400))
+
+const user = {
+    username:'productname',
+    price:299
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user);
+
+let myArr = [1,2,3,4,5]
+function handleArray(anyArray){
+    console.log(`The second value of anyArr is ${anyArray[1]}`); 
+}
+
+// handleArray(myArr)
